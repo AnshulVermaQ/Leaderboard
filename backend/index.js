338a85b1b2c9c39facf901217bc626d00971e216
AnshulVermaQ <146179858+AnshulVermaQ@users.noreply.github.com>
2025-07-19 16:59:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://leaderboard-frontend-m4vn.onrender.com/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
